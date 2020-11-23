54da67931acecc45f4fec5c8b6d00de2aa3d0b8e
Lavesh Salaskar <73989206+Lavesh03@users.noreply.github.com>
2020-11-23 10:49:11 +0530

    Create Swap.cpp

---
+++ Swap.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"\nEnter two numbers : ";
+	cin>>a>>b;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	cout<<"\nAfter swapping numbers are : ";
+	cout<<"\n First Number is "<<a;
+    cout<<"\n Second Number is "<<b;
+	
+	return 0;
+}
