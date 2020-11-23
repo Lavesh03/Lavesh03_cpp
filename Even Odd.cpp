f9a7bf63ac8e813decdac25185b591eefcbdbde5
Lavesh Salaskar <73989206+Lavesh03@users.noreply.github.com>
2020-11-23 10:50:38 +0530

    Create Even Odd.cpp

---
+++ Even Odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int no;
+
+    cout << "Enter an integer: ";
+    cin >> no;
+
+    if ( no % 2 == 0)
+        cout << no << " is even.";
+    else
+        cout << no << " is odd.";
+
+    return 0;
+}
